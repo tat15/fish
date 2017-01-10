7babb366ec8ddf02df0af296b1ad0cc00a0a8380
tat15 <tomtaylor1015@gmail.com>
2017-01-09 19:50:35 -0500

    Update hough_tests.py

--- hough_tests.py
+++ hough_tests.py
@@ -3,52 +3,79 @@ import cv2
 import numpy as np
 
 
-def HoughPrac(i):
+def HoughCirc(i):
     dp = 2
     minDist = 250
     minR = 100
     maxR = 150
-    output = i.copy()
-    edges = cv2.Canny(i, 18, 54)
+    thresh_low = 50 #18 looked ok before
+    thresh_high = 3*thresh_low
+    #output = np.zeros(i.shape)#i.copy()
+    edges = cv2.Canny(i, thresh_low, thresh_high)
     #lines = cv2.HoughLines()
-    circles = cv2.HoughCircles(edges, cv2.HOUGH_GRADIENT, dp, minDist, minRadius=minR, maxRadius=maxR)
+    circles = cv2.HoughCircles(edges, cv2.HOUGH_GRADIENT, dp, minDist, param1 = thresh_low, minRadius=minR, maxRadius=maxR)
 
     if circles is not None:
         circles = np.round(circles[0, :]).astype("int")
-        for (x, y, r) in circles:
-            cv2.circle(output, (x, y), r, (0, 255, 0), 4)
-            cv2.rectangle(output, (x-5, y-5), (x+5, y+5), (0, 128, 255), -1)
-        #cv2.imwrite('./Hough/edges/test_'+str(dp)+'_'+str(minDist)+'_'+str(minR)+'_'+str(maxR)+'.jpg', output)
-    return output
-    #if lines is not None:
-# def LineTest(i):
-#     im_array = cv2.imread('./img_00091.jpg')#[50:, ::]
-#     gray = cv2.cvtColor(im_array, cv2.COLOR_BGR2GRAY)
-#     cv2.imwrite('./Hough/line_test/grey.jpg', gray)
-#     edges = cv2.Canny(gray, 50, 150, apertureSize=3)
-#     lines = cv2.HoughLines(edges, 1, np.pi/180, 200)
-#     for i in lines:
-#         for rho,theta in i:
-#             h, w = gray.shape
-#             h, w = 1000, 1000
-#             a = np.cos(theta)
-#             b = np.sin(theta)
-#             x0 = a*rho
-#             y0 = b*rho
-#             #print('height: ' + str(h) + '\nwidth: ' + str(w) + '\na: ' + str(a) + '\nb: ' + str(b))
-#             x1 = int(x0 + w*(-b))
-#             y1 = int(x0 + h*(a))
-#             x2 = int(x0 - w*(-b))
-#             y2 = int(x0 - h*(a))
+    return circles
 
-#             cv2.line(gray, (x1, y1), (x2, y2), (0,0,255), 2)
-#     cv2.imwrite('./Hough/line_test/edges.jpg', edges)
-#     cv2.imwrite('./Hough/line_test/test.jpg', gray)
+def LineTest(i):
+    im_array = i
 
+    # rho =
+    # theta =
+    # threshold =
+
+    thresh_low = 18
+    thresh_high = 3*thresh_low
+
+    gray = cv2.cvtColor(im_array, cv2.COLOR_BGR2GRAY)
+    edges = cv2.Canny(gray, thresh_low, thresh_high)#, apertureSize=3)
+    return cv2.HoughLinesP(edges, 1, np.pi/180, 200, minLineLength = 25)
+
+def addLines(i, l = None):
+    if l is None:
+        return i
+    else:
+        i2 = i
+
+    for j in l:
+        # for rho,theta in j:
+        #     h, w = i2.shape[:2]
+        #     #h, w = i2.shape[0], i2.shape[1]
+        #     a = np.cos(theta)
+        #     b = np.sin(theta)
+        #     x0 = a*rho
+        #     y0 = b*rho
+        #     #print('height: ' + str(h) + '\nwidth: ' + str(w) + '\na: ' + str(a) + '\nb: ' + str(b))
+        #     x1 = int(x0 + w*(-b))
+        #     y1 = int(x0 + h*(a))
+        #     x2 = int(x0 - w*(-b))
+        #     y2 = int(x0 - h*(a))
+        #     cv2.line(i2, (x1, y1), (x2, y2), (255,0,0), 2)
+        x1, y1, x2, y2 = j[0]
+
+        cv2.line(i2, (x1, y1), (x2, y2), (255, 0, 0), 2)
+    return i2
+    # cv2.imwrite('./Hough/line_test/edges.jpg', edges)
+    # cv2.imwrite('./Hough/line_test/test.jpg', gray)
+
+def addCirc(i, circ):
+    i2 = i
+    for (x, y, r) in circles:
+        cv2.circle(i2, (x, y), r, (255, 255, 255), 4)
+        cv2.rectangle(i2, (x-5, y-5), (x+5, y+5), (255, 128, 255), -1)
+    return i2
 
 n = 0
 for file in os.listdir('./train/LAG/'):
-    img = cv2.imread(os.path.join('./train/LAG/', file), 0)
+    img = cv2.imread(os.path.join('./train/LAG/', file))[50:, ::]#, 0)
     if img is not None:
-        cv2.imwrite('./Hough/edges/large_sample/' + str(n) + '.jpg', HoughPrac(img))#cv2.HoughCircles(img, cv2.HOUGH_GRADIENT, 1, 20))
+        circles = HoughCirc(img)
+        lines = LineTest(img)
+
+        img = addLines(img, lines)
+        img = addCirc(img, circles)
+
+        cv2.imwrite('./Hough/edges/large_sample/' + str(n) + '.jpg', img)#cv2.HoughCircles(img, cv2.HOUGH_GRADIENT, 1, 20))
         n+=1
